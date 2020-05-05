822873b97ade6a56b8bf651a3c18642d9c62818b
rafa díaz <rafa daz@DESKTOP-FPIAHP8>
2020-05-06 00:09:55 +0200

    ME HE TRAIDO TODOS LOS CAMBIOS DE MASTER

---
+++ Synistry/src/main/java/aiss/model/giphy/Downsized.java
@@ -0,0 +1,83 @@
+
+package aiss.model.giphy;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "height",
+    "size",
+    "url",
+    "width"
+})
+public class Downsized {
+
+    @JsonProperty("height")
+    private String height;
+    @JsonProperty("size")
+    private String size;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("width")
+    private String width;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("height")
+    public String getHeight() {
+        return height;
+    }
+
+    @JsonProperty("height")
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    @JsonProperty("size")
+    public String getSize() {
+        return size;
+    }
+
+    @JsonProperty("size")
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("width")
+    public String getWidth() {
+        return width;
+    }
+
+    @JsonProperty("width")
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
